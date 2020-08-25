fdc507864268a1dd5939f1a03addbab585f32cec
danvixent <danvixent@gmail.com>
2020-08-25 17:13:55 +0100

    fixed sort algorithm

--- insertion-sort/main.go
+++ insertion-sort/main.go
@@ -1,7 +1,9 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(sort([]int{5, 4, 3, 6, 2, 1, -2, -8, 1002, 99, 22, 32, 435, 5546, 24311, 4442, 53932, 4245, 242421}))
 }
 
 func sort(slice []int) []int {
@@ -9,13 +11,14 @@ func sort(slice []int) []int {
 		return slice
 	}
 
-	i, j := 0, 1
-	for {
+	for j := 1; j < len(slice); j++ {
+		i := j - 1
 		key := slice[j]
 		for i > -1 && slice[i] > key {
 			slice[i+1] = slice[i]
+			i--
 		}
-		// slice
-		j++
+		slice[i+1] = key
 	}
+	return slice
 }
